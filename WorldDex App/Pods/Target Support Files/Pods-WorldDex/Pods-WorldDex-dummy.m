eef36dd1dda7789ec69ba94edc47194f7ab7dbdb
Anthony Qin <antqin27@stanford.edu>
2023-10-27 17:48:05 -0700

    Add iOS app

---
+++ WorldDex App/Pods/Target Support Files/Pods-WorldDex/Pods-WorldDex-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WorldDex : NSObject
+@end
+@implementation PodsDummy_Pods_WorldDex
+@end
---
+++ WorldDex App/WorldDex/WorldDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldDexApp.swift
+//  WorldDex
+//
+//  Created by Anthony Qin on 10/27/23.
+//
+
+import SwiftUI
+
+@main
+struct WorldDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
