a951be59b72dd9a6a148cd360cdcb958b5a73e8e
Andrew Liu <andelerliutv@gmail.com>
2023-10-29 09:13:19 -0700

    a working version of the web app

--- worlddex/worlddex/worlddex.py
+++ worlddex/worlddex/worlddex.py
@@ -1,28 +1,153 @@
 """Welcome to Reflex!."""
 import reflex as rx
-
-from worlddex.state import State
+from worlddex.state import ImageState
 from worlddex.pages.dashboard import dashboard
 import requests as requests
 
 
-color = "rgb(107,99,246)"
 
 def index():
     """The main view."""
     return rx.vstack(
-        rx.text(State.username),
-        rx.box(
-            rx.button(
-                rx.link('Login', href='/dashboard')
-            )
-        )
+        rx.center(
+            rx.image(
+                src='AppIcon2.jpg',
+                border_radius="80px",
+                width="50%",
+                height="auto"
+            ),
+            height="500px",
+            width="50%",
+            style={"overflow": "hidden"},
+            margin_top="100px",
+            # border_width="thin",
+            # border_color="green",
+        ),
+        rx.vstack(
+            rx.vstack(
+                rx.input(
+                    placeholder="Username",
+                    size="md",
+                    bg="white",
+                    font_color="#5C4033",
+                    value=ImageState.username,
+                    on_change=ImageState.setUsername
+                ),
+                rx.input(
+                    placeholder="Password",
+                    size="md",
+                    bg="white",
+                    font_color="#5C4033",
+                    value=ImageState.password,
+                    on_change=ImageState.setPassword
+                ),
+                rx.box(
+                    rx.button(
+                        rx.link('Login', href='/dashboard'),
+                        bg="white",
+                        on_click=lambda: ImageState.login()
+                    ),
+                ),
+                rx.box(
+                    rx.link(
+                        "Create an Account",
+                        href='/makeAccount',
+                    ),
+                ),
+                margin="50px",
+                # border_width="thin",
+                # border_color="green",
+            ),
+        
+        ),
+        bg="#ece0d1",
+        height="1120px"
+        
     )
 
-images = [1, 2, 3, 4, 5, 6, 7]
 def small_box(image):
-    return rx.box(image, height="75px",width="100%",bg="green")
+    return rx.center(
+        
+        rx.image(src=(image['image']), style={"max-width": "100%", "max-height": "100%"}),
+        on_click=lambda: ImageState.update_current_image(image),
+        cursor="pointer",
+        height="100px",
+        width="100%",
+        border_radius="10px",
+        bg="white",
+        _hover={
+        "transform": "scale(1.1)",
+        "transition": "transform .2s"
+        },
+        style={"overflow": "hidden"},
+        padding="5px",
+        box_shadow="lg"
+    )
 
+def makeAccount():
+    return rx.vstack(
+        rx.center(
+            rx.image(
+                src='/AppIcon.jpg',
+                border_radius="80px",
+                width="50%",
+                height="auto"
+            ),
+            height="500px",
+            width="50%",
+            style={"overflow": "hidden"},
+            margin_top="100px",
+        ),
+        rx.vstack(
+            rx.vstack(
+                rx.input(
+                    placeholder="Enter Username",
+                    size="md",
+                    bg="white",
+                    font_color="#5C4033",
+                    value=ImageState.username,
+                    on_change=ImageState.setUsername
+                ),
+                rx.input(
+                    placeholder="Enter Password",
+                    size="md",
+                    bg="white",
+                    font_color="#5C4033",
+                    value=ImageState.password,
+                    on_change=ImageState.setPassword
+                ),
+                rx.input(
+                    placeholder="Enter Email",
+                    size="md",
+                    bg="white",
+                    font_color="#5C4033",
+                    value=ImageState.email,
+                    on_change=ImageState.setEmail
+                ),
+                rx.box(
+                    rx.button(
+                        rx.link('Create', href='/dashboard'),
+                        bg="white",
+                        on_click=lambda: ImageState.newUser(),
+                    ),
+                ),
+                rx.box(
+                    rx.link(
+                        "Back to Login",
+                        href='/',
+                    ),
+                ),
+                margin="20px",
+                # border_width="thin",
+                # border_color="green",
+            ),
+        ),
+        bg="#ece0d1",
+        height="1120px"
+    )
+
+
+@rx.page(on_load=ImageState.fetch_data())
 def dashboard():
     """The dashboard page.
 
@@ -31,39 +156,48 @@ def dashboard():
     """
     return rx.hstack(
         rx.vstack(
-            rx.box(
-                "full size image",
-                margin_top="100px",
-                border_width="thin",
-                border_color="green",
-                width="85%",
-                height="750px",
-            ),
             rx.flex(
                 rx.text(
-                    "location"
+                    ImageState.currentLocation,
+                    as_='b',
+                    font_size="1.5em"
                 ),
                 rx.spacer(),
                 rx.text(
-                    "time"
+                    ImageState.currentTime,
+                    font_size="1.5em",
+                    as_='b'
                 ),
-                margin_top="10px",
-                border_width="thin",
-                border_color="green",
-                width="85%",
+                margin_top="100px",
+                # border_width="thin",
+                # border_color="green",
+                width="68%",
                 height="35px",
             ),
+            rx.center(
+                rx.image(src=ImageState.currentImage, style={"max-width": "100%", "max-height": "100%"}),
+                margin_top="100px",
+                border_width="medium",
+                border_color="#5C4033",
+                border_radius="20px",
+                bg="white",
+                width="70%",
+                height="750px",
+                padding="20px"
+            ),
             rx.box(
-                "caption",
+                rx.text(
+                    ImageState.currentCaption
+                ),
                 margin_top="20px",
-                border_width="thin",
-                border_color="green",
-                width="85%",
+                # border_width="thin",
+                # border_color="green",
+                width="68%",
                 height="150px",
             ),
             "Main Image Page",
-            border_width="thin",
-            border_color="green",
+            # border_width="thin",
+            # border_color="green",
             width="85%",
             height="1120px",
         ),
@@ -73,37 +207,38 @@ def dashboard():
         #     bg="black",
         # ),
         rx.vstack(
-            rx.box(
+            rx.center(
                 rx.text(
-                    "Other Collectables!",
-                    font_size='2em',
+                    ImageState.username + "'s WorldDex!",
+                    font_family="trebuchet ms",
+                    font_size='3em',
+                    as_='b'
                 ),
                 margin_top="85px",
             ),
             rx.box(
                 rx.responsive_grid(
-                    rx.foreach(images, lambda image: small_box(image)),
-                    columns=[3],
+                    rx.foreach(ImageState.img, lambda image: small_box(image)),
+                    columns=[4],
                     spacing="4",
                 ),
                 margin_top="100px",
-                border_width="thin",
-                border_color="green",
+                # border_width="thin",
+                # border_color="green",
                 width="85%",
                 height="650px",
             ),
             "Library of other images",
-            border_width="thin",
-            border_color="green",
-            width="40%",
+            # border_width="thin",
+            # border_color="green",
+            width="70%",
             height="1120px",
             
         ),
-        margin="auto",
-        width="80%",
+        margin_right="25%",
+        width="100%",
         height="1120px",
-        border_width="thick",
-        border_color="green",
+        bg="#ece0d1"
     )
     
 
@@ -111,4 +246,5 @@ def dashboard():
 app = rx.App()
 app.add_page(index)
 app.add_page(dashboard, route='/dashboard')
+app.add_page(makeAccount, route='/makeAccount')
 app.compile() 
